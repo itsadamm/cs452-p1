cba35ba9cea4cf98913be45cce2945704150981e
Adam Adri <adamadri@u.boisestate.edu>
2024-09-23 15:22:46 -0600

    new main commit

--- app/main.c
+++ app/main.c
@@ -1,7 +1,37 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <unistd.h>
 
-int main(void)
+
+int main(int argc, char **argv)
 {
   printf("hello world\n");
+  int c;
+
+  while((c = getopt (argc, argv, "abc:")) != -1) {
+    switch(c)
+    {
+      case 'a':
+        printf("get a here");
+        break;
+      case 'b':
+        printf("get b here");
+        break;
+      case 'c':
+        printf("get c here");
+        break;
+      case '?':
+        if (isprint(optopt)){
+          fprintf(stderr, "Unknown option", optopt);
+        }else {
+          fprintf(stderr, "Unknown option", optopt);
+        }
+      return 1;
+      default:
+        abort();
+        break;
+    }
+  }
   return 0;
 }
